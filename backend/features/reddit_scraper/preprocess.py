c3fe034a0a6a271ef01c561384a9969dc28407e0
Prathamesh Mundada <mundadaprathamesh@gmail.com>
2025-09-20 15:42:39 +0530

    fixed folder structure

--- backend/features/reddit_scraper/preprocess.py
+++
@@ -1,6 +0,0 @@
-def preprocess_reddit_data(posts):
-    text_corpus = []
-    for post in posts:
-        text_corpus.append(post["title"])
-        text_corpus.extend(post["comments"])
-    return " ".join(text_corpus[:2000])  # truncate for MVP
